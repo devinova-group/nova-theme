fe807395f3966e7f8d96fb40113a9ecd8af5d3d6
lazar93kg <lazar93kg@gmail.com>
2023-01-05 18:40:51 +0100

    1.0.8 Table Hook Test

--- src/library/Table.tsx
+++ src/library/Table.tsx
@@ -3,6 +3,7 @@ import { Box } from "theme-ui";
 
 import { BoxProps as TableThemeUI } from "theme-ui";
 import useSortableData from "../hooks/useSortableData";
+import Field from "./Field";
 
 export interface TableProps extends TableThemeUI {
   children?: React.ReactNode;
@@ -12,82 +13,59 @@ export interface TableProps extends TableThemeUI {
 }
 
 const Table = (props: TableProps) => {
-  const { items, requestSort, sortConfig } = useSortableData(
-    props.data,
-    "ascending"
-  );
+  const { items, requestSort, sortConfig } = useSortableData(props.data, {
+    key: "name",
+    direction: "ascending",
+  });
+
   return (
-    <Box>
-      {props.children}
-      <Box
-        {...props}
-        sx={{ alignItems: "center", display: "flex", justifyContent: "center" }}
-      >
-        <table style={{ width: "80%" }}>
-          <thead style={{ textAlign: "left" }}>
-            <tr>
-              <th onClick={() => requestSort("name")}>Name</th>
-              <th onClick={() => requestSort("age")}>Age</th>
-              <th onClick={() => requestSort("address")}>Address</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody style={{ textAlign: "left" }}>
-            {items.map((data: any) => (
-              <tr key={data.key}>
-                <td>{data.name}</td>
-                <td>{data.age}</td>
-                <td>{data.address}</td>
-                <td>
-                  <a href="#">Delete</a>
-                </td>
+    <>
+      <Box>
+        {props.children}
+        <Box
+          {...props}
+          sx={{
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <table style={{ width: "80%" }}>
+            <thead style={{ textAlign: "left" }}>
+              <tr style={{ cursor: "pointer" }}>
+                {props.columns &&
+                  props.columns.map((column: any, index: number) => (
+                    <th
+                      onClick={() =>
+                        requestSort(`${column.title.toLowerCase()}`)
+                      }
+                      key={index}
+                    >
+                      {column.title}
+                      {/* Partly working */}
+                      {sortConfig.direction === "ascending" ? "⬇️" : "⬆️"}
+                    </th>
+                  ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody style={{ textAlign: "left" }}>
+              {items &&
+                items.map((data: any) => (
+                  <tr key={data.key}>
+                    <td>{data.name}</td>
+                    <td>{data.age}</td>
+                    <td>{data.address}</td>
+                    <td>
+                      <a href="#">Delete</a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
 export default Table;
-
-/* onClick={() => handleClick("name")} */
-/* onClick={() => handleNumber("age")} */
-/* onClick={() => handleClick("address")} */
-/* onClick={() => handleDelete(data)} */
-/*   const [newData, setNewData] = useState(props.data);
-  const [order, setOrder] = useState("ASC");
-  const [orderNumber, setOrderNumber] = useState("1"); */
-
-/* const handleDelete = (data: any) => {
-    console.log(data);
-  };
-  const handleNumber = (col: any) => {
-    if (orderNumber === "1") {
-      const sorded = [...newData].sort((a, b) => (a.age > b.age ? 1 : -1));
-      setOrderNumber("2");
-      setNewData(sorded);
-    }
-    if (orderNumber === "2") {
-      const sorded = [...newData].sort((a, b) => (a.age < b.age ? 1 : -1));
-      setOrderNumber("1");
-      setNewData(sorded);
-    }
-  };
-  const handleClick = (col: any) => {
-    if (order === "ASC") {
-      const sorded = [...newData].sort((a, b) =>
-        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
-      );
-      setNewData(sorded);
-      setOrder("DSC");
-    }
-    if (order === "DSC") {
-      const sorded = [...newData].sort((a, b) =>
-        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
-      );
-      setNewData(sorded);
-      setOrder("ASC");
-    }
-  }; */
